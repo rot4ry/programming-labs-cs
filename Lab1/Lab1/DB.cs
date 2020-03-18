a4f444e6080a7ac15f673de7d9a4823832da16f9
rot4ry <wojtekw233@gmail.com>
2020-03-18 22:12:53 +0100

    Klasy w oddzielnych plikach

---
+++ Lab1/Lab1/DB.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace Lab1
+{
+    public class DB
+    {
+        public void Select(SqlConnection connection)
+        {
+            var query = "SELECT * FROM Customers";
+            var command = new SqlCommand(query, connection);
+            var reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                //  Console.WriteLine(reader.["CompanyName"]);
+            }
+        }
+
+        public void Insert(SqlConnection connection, int id, string description)
+        {
+            var query = "INSERT INTO region(regionId, regionDescription)"
+                + "VALUES (@id, '@description')";
+
+            var command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@id", id);
+            command.Parameters.AddWithValue("@description", description);
+
+            var affected = command.ExecuteNonQuery();
+            Console.WriteLine($"{affected} rows affected");
+        }
+
+        public void Update(SqlConnection connection, int id, string description)
+        {
+            var query = "UPDATE region(regionId, regionDescription)"
+                + $"SET regionDescription = {description}"
+                + $"WHERE @id = {id}";
+
+            var command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@id", id);
+            command.Parameters.AddWithValue("@description", description);
+
+            var affected = command.ExecuteNonQuery();
+            Console.WriteLine($"{affected} rows affected");
+        }
+
+        public void Delete(SqlConnection connection, int id)
+        {
+            var query = "DELETE FROM region(regionId, regionDescription)"
+                    + $"WHERE @id = {id}";
+
+            var command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("id", id);
+
+            var affected = command.ExecuteNonQuery();
+            Console.WriteLine($"{affected} rows affected");
+        }
+    }
+}
--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -4,60 +4,7 @@ using System.Data.SqlClient;
 
 namespace Lab1
 {
-    public class DB
-    {
-        public void Select(SqlConnection connection)
-        {
-            var query = "SELECT * FROM Customers";
-            var command = new SqlCommand(query, connection);
-            var reader = command.ExecuteReader();
-
-            while (reader.Read())
-            {
-                //  Console.WriteLine(reader.["CompanyName"]);
-            }
-        }
-
-        public void Insert(SqlConnection connection, int id, string description)
-        {
-            var query = "INSERT INTO region(regionId, regionDescription)"
-                + "VALUES (@id, '@description')";
-            
-            var command = new SqlCommand(query, connection);
-            command.Parameters.AddWithValue("@id", id);
-            command.Parameters.AddWithValue("@description", description);
-
-            var affected = command.ExecuteNonQuery();
-            Console.WriteLine($"{affected} rows affected");
-        }
-
-        public void Update(SqlConnection connection, int id, string description)
-        {
-            var query = "UPDATE region(regionId, regionDescription)"
-                + $"SET regionDescription = {description}"
-                + $"WHERE @id = {id}";
-
-            var command = new SqlCommand(query, connection);
-            command.Parameters.AddWithValue("@id", id);
-            command.Parameters.AddWithValue("@description", description);
-
-            var affected = command.ExecuteNonQuery();
-            Console.WriteLine($"{affected} rows affected");
-        }
-
-        public void Delete(SqlConnection connection, int id)
-        {
-            var query = "DELETE FROM region(regionId, regionDescription)"
-                    + $"WHERE @id = {id}";
-
-            var command = new SqlCommand(query, connection);
-            command.Parameters.AddWithValue("id", id);
-
-            var affected = command.ExecuteNonQuery();
-            Console.WriteLine($"{affected} rows affected");
-        }
-    }
-
+    
     public class Program
     {
         //CRUD
