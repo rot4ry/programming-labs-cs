8918c43bd1917a90c0dde9e02d6653f329e2e273
rot4ry <wojtekw233@gmail.com>
2020-03-18 22:14:00 +0100

    Dodanie Lab4

---
+++ Lab4/Lab4/Program.cs
@@ -0,0 +1,41 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            var sw = new Stopwatch();
+            //var google = new RestClient("http://google.pl");
+            //var ath = new RestClient("http://ath.bielsko.pl");
+            //var otomoto = new RestClient("http://otomoto.pl");
+            //var stackoverflow = new RestClient("https://stackoverflow.com");
+
+            sw.Start();
+            List <Task> tasks  = new List<Task>();
+            //ExecuteAsync nie czeka na odpowiedź na żądanie
+            //  await - nie zawsze
+
+
+            tasks.Add(Website.Download("http://google.pl", "/"));
+            Console.WriteLine(sw.Elapsed);
+            tasks.Add(Website.Download("http://otomoto.pl", "/"));
+            Console.WriteLine(sw.Elapsed);
+            tasks.Add(Website.Download("https://stackoverflow.com", "/"));
+            Console.WriteLine(sw.Elapsed);
+         
+            Console.WriteLine("___________");
+         
+            await Task.WhenAny(tasks);
+            Console.WriteLine(sw.Elapsed);
+            await Task.WhenAll(tasks);
+            Console.WriteLine(sw.Elapsed);
+            sw.Stop();
+        }
+    }
+}
---
+++ Lab4/Lab4/Website.cs
@@ -0,0 +1,18 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    public class Website
+    {
+        public static async Task<IRestResponse> Download(string domain, string path, Method method = Method.GET)
+        {
+            var client = new RestClient(domain);
+            return await client.ExecuteAsync(new RestRequest(path), method);
+            
+        }
+    }
+}
