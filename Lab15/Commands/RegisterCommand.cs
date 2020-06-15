bb5580b859a8ab6c779fce6c78ffec338ca9cbfb
rot4ry <wojtekw233@gmail.com>
2020-06-15 13:42:43 +0200

    Added Lab15

---
+++ Lab15/Commands/RegisterCommand.cs
@@ -0,0 +1,41 @@
+﻿using Lab15.Viewmodels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Lab15.Commands
+{
+    public class RegisterCommand : ICommand
+    {
+
+        private RegisterViewModelValidator _validator = new RegisterViewModelValidator();
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            var model = parameter as RegisterViewModel;
+            if (model is null)
+            {
+                return false;                
+            }
+            var results = _validator.Validate(model);
+            return results.IsValid;
+        }
+
+        public void Execute(object parameter)
+        {
+            var model = parameter as RegisterViewModel;
+            MessageBox.Show($"Zarejestrowano użytkownika {model.Email}", "Powodzenie", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
