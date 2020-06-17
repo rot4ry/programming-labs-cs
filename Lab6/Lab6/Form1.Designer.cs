485064f9408e8fc28a439cfffe119dca51c5637d
rot4ry <wojtekw233@gmail.com>
2020-06-17 18:34:39 +0200

    Added Lab6

---
+++ Lab6/Lab6/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Lab6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(197, 414);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(322, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(164, 59);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Choose file";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(519, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(269, 414);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(322, 77);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(164, 59);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Start";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 150F);
+            this.textBox1.Location = new System.Drawing.Point(287, 159);
+            this.textBox1.MinimumSize = new System.Drawing.Size(4, 200);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(226, 234);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "00";
+            this.textBox1.Visible = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.DefaultExt = "\".lotto\"";
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Lab6/Lab6/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.ComponentModel;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace Lab6
+{
+    public partial class Form1 : Form
+    {
+        private int _counter = 0;
+        private BindingList<Number> numberList = new BindingList<Number>();
+        private List<int> availableNumbersList = Enumerable.Range(1, 100).ToList();
+        private Random randomizer = new Random();
+        
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridView1.DataSource = numberList;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            _counter = 0;
+            timer1.Start();
+            dataGridView1.Rows.Clear();
+        }
+
+        private TextBox GenerateTextBox(string text)
+        {
+            return new TextBox()
+            {
+                Text = text,
+                Width = 100,
+                ReadOnly = true
+            };
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int randomNumber = availableNumbersList[randomizer.Next(availableNumbersList.Count())];
+
+            textBox1.Text = randomNumber.ToString();
+            _counter++;
+
+            if(_counter%10 == 0)
+            {
+                numberList.Add(
+                    new Number
+                    {
+                        Value = randomNumber.ToString()
+                    });
+                availableNumbersList.Remove(randomNumber);
+            }
+
+            if(_counter == 60)
+            {
+                timer1.Stop();
+            }
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            flowLayoutPanel1.Controls.Clear();
+            string filePath = openFileDialog1.FileName;
+            string fileContent = File.ReadAllText(filePath);
+
+            var formattedFileContent = fileContent.Split(new[] { "\r", "\n", " ", "\t" },
+                                        StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string item in formattedFileContent)
+            {
+                flowLayoutPanel1.Controls.Add(GenerateTextBox(item.ToString()));
+            }
+            textBox1.Visible = true;
+            button2.Visible = true;
+        }
+    }
+}
